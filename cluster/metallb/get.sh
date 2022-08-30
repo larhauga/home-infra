4098095f4e0672819697b7dbec60c4307df7f35a
Lars Haugan <lars@larshaugan.net>
2022-08-30 06:03:09 +0000

    update metallb

--- cluster/metallb/get.sh
+++ cluster/metallb/get.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-curl --fail -SsLO https://raw.githubusercontent.com/danderson/metallb/master/manifests/metallb.yaml
+#curl --fail -SsLO https://raw.githubusercontent.com/danderson/metallb/master/manifests/metallb.yaml
+
+curl --fail -sSLo metallb.yaml https://raw.githubusercontent.com/metallb/metallb/v0.13.4/config/manifests/metallb-native.yaml
+
